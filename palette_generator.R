b32c22fb6391058db4ae2f1f4eb4df8a6259809d
Brenna Kelly <brennackelly@icloud.com>
2024-09-24 17:33:15 -0600

    tidying

--- palette_generator.R
+++ palette_generator.R
@@ -8,32 +8,26 @@ white <- "white"
 
 col_white_col <- colorRampPalette(c(color_x, white, color_y), space = "rgb",
                                       interpolate = "linear", alpha = FALSE)
-col_white_col(7)
 
 col_corner_col <- colorRampPalette(c(color_x, corner, color_y), space = "rgb",
                                    interpolate = "linear", alpha = FALSE)
-col_corner_col(7)
 
-
+# -1 diagonal
 col_col <- colorRampPalette(c(color_x, color_y), space = "rgb",
                             interpolate = "linear", alpha = FALSE)
-
-col_col(4)[2:3] # -1 diagonal
 
 diag_x_lower <- col_white_col(7)[6]
 diag_y_lower <- col_white_col(5)[2]
 
-
 diag_lower <- colorRampPalette(c(diag_x_lower, diag_y_lower), space = "rgb",
                                interpolate = "linear", alpha = FALSE)
-diag_lower(3)[2]
 
 diag_x_upper <- col_corner_col(7)[5]
 diag_y_upper <- col_corner_col(7)[3]
 
+# +1 diagonal
 diag_upper <- colorRampPalette(c(diag_x_upper, diag_y_upper), space = "rgb",
                                interpolate = "linear", alpha = FALSE)
-diag_upper(3)[2] # +1 diagonal
 
 
 r4 <- c(color_x, col_corner_col(7)[2], col_corner_col(7)[3], col_corner_col(7)[4])
